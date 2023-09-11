c3d39afb3d240128af51250d8b98998af3c4c5a4
JuanSustaita <91509288+JuanSustaita@users.noreply.github.com>
2023-09-11 11:22:22 -0400

    Asked what they would like to order

--- M2T2_Sustaita/M2T2_Sustaita/main.cpp
+++ M2T2_Sustaita/M2T2_Sustaita/main.cpp
@@ -2,22 +2,37 @@
 #include <iomanip>
 
 using namespace std;
-
 /*
 CSC 134
 M2T1 - Receipt Calculator V2
 SustaitaJ
 9/6/23
-
-
 user should enter name of food item, price, and quantity
-
 */
 
 int main()
 {
-    double menu_price = 5.99;
+    double menu_price = 0;
     double tax_percent = 0.07; // Cumberland County Tax
+    double item_price;
+    int num_items;
+    string item_name;
+
+    // Step 1 - greet user
+    cout << "Welcome to the Grill" << endl;
+    // Step 2 - ask user for their order
+    cout << "What would you like today?" << endl;
+    cin >> item_name;
+
+    cout << "What's the price on the item? " << endl;
+    cin >> item_price;
+
+    cout << "How many " << item_name << "(s) do you want? " << endl;
+    cin >> num_items;
+
+    // Step 3 - Calculate the menu price
+    menu_price = num_items * item_price;
+
     double tax_dollars;
     double total_price; // price including tax
 
@@ -25,6 +40,8 @@ int main()
     total_price = menu_price + tax_dollars; // Calculates menu price and tax together
 
     cout << fixed << setprecision(2) << endl;
+    // print the receipt
+    cout << "Order: " << num_items << " " << item_name << "(s)" << endl;
     cout << "Menu price is: $" << menu_price << endl;
     cout << "Tax total is:  " << tax_dollars << endl;
     cout << "__________________" << endl;
@@ -32,4 +49,3 @@ int main()
 
     return 0;
 }
-
